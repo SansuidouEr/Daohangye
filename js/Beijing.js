04c364b802d3ca135d61d09c1569c8c99ef4e3f5
SansuidouEr <2898865071@qq.com>
2021-12-16 10:11:26 +0000

    更新背景高度为1130

--- js/Beijing.js
+++ js/Beijing.js
@@ -1474,7 +1474,7 @@ window.particlesJS = function(tag_id, params){
 
   /* 画布大小 */
   canvas_el.style.width = "100%";
-  canvas_el.style.height = "1085px";
+  canvas_el.style.height = "1130px";
 
   /* append canvas */
   var canvas = document.getElementById(tag_id).appendChild(canvas_el);
